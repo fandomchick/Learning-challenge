f6eb1a48175215312cbdcf2c7ffad1b28e8413c5
Myrthe <74498012+fandomchick@users.noreply.github.com>
2021-03-24 14:57:54 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿
+namespace Melearning
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.question = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.magic = new System.Windows.Forms.Label();
+            this.btnstart = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // question
+            // 
+            this.question.Location = new System.Drawing.Point(152, 255);
+            this.question.Name = "question";
+            this.question.Size = new System.Drawing.Size(531, 27);
+            this.question.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(265, 207);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(258, 26);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "write your question down.";
+            // 
+            // magic
+            // 
+            this.magic.AutoSize = true;
+            this.magic.Font = new System.Drawing.Font("Times New Roman", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.magic.Location = new System.Drawing.Point(296, 97);
+            this.magic.Name = "magic";
+            this.magic.Size = new System.Drawing.Size(195, 42);
+            this.magic.TabIndex = 2;
+            this.magic.Text = "magic 8 ball";
+            this.magic.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // btnstart
+            // 
+            this.btnstart.Location = new System.Drawing.Point(515, 316);
+            this.btnstart.Name = "btnstart";
+            this.btnstart.Size = new System.Drawing.Size(183, 45);
+            this.btnstart.TabIndex = 3;
+            this.btnstart.Text = "start";
+            this.btnstart.UseVisualStyleBackColor = true;
+            this.btnstart.Click += new System.EventHandler(this.btnstart_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(378, 161);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "...";
+            this.label2.Click += new System.EventHandler(this.label2_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnstart);
+            this.Controls.Add(this.magic);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.question);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox question;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label magic;
+        private System.Windows.Forms.Button btnstart;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Melearning
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label2.Hide();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnstart_Click(object sender, EventArgs e)
+        {
+                magic.Hide();
+                label1.Hide();
+                label2.Show();
+                int rannum = 0;
+                Random r = new Random();
+
+                rannum = r.Next(1, 4);
+
+                switch (rannum)
+                {
+                     case 1:
+                        label2.Text = "Answer: " +  "YES";
+                        break;
+                    case 2:
+                        label2.Text = "Answer: " +  "NO";
+                        break;
+                    case 3:
+                        label2.Text = "Answer: " +  "Ask later again";
+                        break;
+                    default:
+                        label2.Text = "Answer: " + "Not likely too";
+                         break;
+
+            }
+            }
+
+        private void label2_Click_1(object sender, EventArgs e)
+        {
+            
+            
+            
+
+        }
+    }
+}
+
